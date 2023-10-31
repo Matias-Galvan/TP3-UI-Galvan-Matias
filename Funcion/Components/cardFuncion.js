2831ce61637acc949cef8b012178c3e6accc4aca
Matias-Galvan <matiasgalvan90@gmail.com>
2023-10-31 02:22:52 -0300

    andando el GetFunciones

--- Funcion/Components/cardFuncion.js
+++ Funcion/Components/cardFuncion.js
@@ -3,8 +3,6 @@ export const cardFuncion = (peliculaId, titulo, poster) => {
         <div class="card shadow-sm" id="${peliculaId}">
             <img src="${poster}">
           <div class="card-body">
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.</p>
               <h2>${titulo}</h2>
             <div class="d-flex justify-content-between align-items-center">
               <div class="btn-group">
